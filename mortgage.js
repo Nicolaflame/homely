d8606d83250a04be8fdb7ec950b6135df8cb46cc
Nicolaflame <144032765+Nicolaflame@users.noreply.github.com>
2023-11-16 09:00:47 -0500

    prelim JS added

--- mortgage.js
+++ mortgage.js
@@ -1,7 +1,19 @@
 "use strict";
 
-// define variables
+// Defining variables
+
+const homePrice = 5000;
+const downPmt = 2000;
+// const homePrice = document.querySelector(".home-price");
+// const downPmt = document.querySelector(".down-pmt");
+const term = document.querySelector(".term");
+var i = document.querySelector(".rate");
+
+var p = homePrice - downPmt;
+var n = term * 12;
+console.log(p);
 
 // mortgage formula = principal & Interest
+// P = L[c(1 + c)n]/[(1 + c)n - 1]
 
 // property tax calculator ($2.248 per $100 of assessed value)
